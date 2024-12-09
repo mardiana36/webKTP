89baa64fed2e47606e391b3bc4fb5a3b4a086493
triana <agungtrianaputra22@gmail.com>
2024-12-09 21:34:36 +0800

    perbaikan path

--- sourceCode/app/controllers/adminPembuatanController.php
+++ sourceCode/app/controllers/adminPembuatanController.php
@@ -29,11 +29,25 @@ class adminPembuatanController
             $pathTtd = $_POST['pathTtd'];
             $status = $_POST['status'];
 
+            $stmt = $this->adminPembuatan->showPembuatan($id);
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $pathFoto = $this->processFileUpload('pathFoto', $data['pathFoto']); 
+            if ($pathKK === false) {
+            echo "<script>alert('Terjadi kesalahan saat upload foto KK!');</script>";
+            return; 
+            }
+
+             $pathTtd = $this->processFileUpload('pathTtd', $data['pathTtd']);
+             if ($pathRekumendasi === false) {
+            echo "<script>alert('Terjadi kesalahan saat upload foto Rekomendasi!');</script>";
+            return;
+            }
+
             $this->adminPembuatan->id = $id;
             $this->adminPembuatan->pathFoto = $pathFoto;
             $this->adminPembuatan->pathTtd = $pathTtd;
             $this->adminPembuatan->status = $status;
-
 
             if ($this->adminPembuatan->updatePembuatan()) {
                 echo "<script>window.location.href = 'index.php?action=radminPembuatan';</script>";
@@ -48,6 +62,8 @@ class adminPembuatanController
             } else {
                 echo "User not found.";
             }
+
+
         }
     }
 
@@ -73,6 +89,30 @@ class adminPembuatanController
             } else {
                 echo "User not found.";
             }
+        }
+    }
+
+    private function processFileUpload($fieldName, $oldFilePath) {
+        if ($_FILES[$fieldName]['error'] === UPLOAD_ERR_OK) {
+            $target_dir = "app/views/assets/images/foto/";
+            $target_file = $target_dir . basename($_FILES[$fieldName]["name"]);
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            var_dump($_FILES);
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                echo "<script>alert('Tipe file $fieldName tidak valid!');</script>";
+                return false;
+            } elseif ($_FILES[$fieldName]["size"] > 2000000) {
+                echo "<script>alert('Ukuran file $fieldName terlalu besar!');</script>";
+                return false;
+            } else {
+                if (move_uploaded_file($_FILES[$fieldName]["tmp_name"], $target_file)) {
+                    return basename($_FILES[$fieldName]["name"]); 
+                } else {
+                    return false; 
+                }
+            }
+        } else {
+            return $oldFilePath; 
         }
     }
 
--- sourceCode/app/controllers/adminPengajuanController.php
+++ sourceCode/app/controllers/adminPengajuanController.php
@@ -23,53 +23,66 @@ class adminPengajuanController
         include "app/views/adminPengajuan/index.php";
     }
 
-    public function update($id)
-    {
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
-            $nama = $_POST['nama'];
-            $jk = $_POST['jk'];
-            $nik = $_POST['nik'];
-            $tmpLahir = $_POST['tmpLahir'];
-            $tglLahir = $_POST['tglLahir'];
-            $alamat = $_POST['alamat'];
-            $agama = $_POST['agama'];
-            $statusPerkawinan = $_POST['statusPerkawinan'];
-            $pekerjaan = $_POST['pekerjaan'];
-            $negara = $_POST['negara'];
-            $golDarah = $_POST['golDarah'];
-            $pathKK = $_POST['pathKK'];
-            $pathRekumendasi = $_POST['pathRekumendasi'];
+public function update($id)
+{
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $nama = $_POST['nama'];
+        $jk = $_POST['jk'];
+        $nik = $_POST['nik'];
+        $tmpLahir = $_POST['tmpLahir'];
+        $tglLahir = $_POST['tglLahir'];
+        $alamat = $_POST['alamat'];
+        $agama = $_POST['agama'];
+        $statusPerkawinan = $_POST['statusPerkawinan'];
+        $pekerjaan = $_POST['pekerjaan'];
+        $negara = $_POST['negara'];
+        $golDarah = $_POST['golDarah'];
 
-            $this->adminPengajuan->id = $id;
-            $this->adminPengajuan->nama = $nama;
-            $this->adminPengajuan->jk = $jk;
-            $this->adminPengajuan->nik = $nik;
-            $this->adminPengajuan->tmpLahir = $tmpLahir;
-            $this->adminPengajuan->tglLahir = $tglLahir;
-            $this->adminPengajuan->alamat = $alamat;
-            $this->adminPengajuan->agama = $agama;
-            $this->adminPengajuan->statusPerkawinan = $statusPerkawinan;
-            $this->adminPengajuan->pekerjaan = $pekerjaan;
-            $this->adminPengajuan->negara = $negara;
-            $this->adminPengajuan->golDarah = $golDarah;
-            $this->adminPengajuan->pathKK = $pathKK;
-            $this->adminPengajuan->pathRekumendasi = $pathRekumendasi;
+        $stmt = $this->adminPengajuan->showPengajuan($id);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            if ($this->adminPengajuan->updatePengajuan()) {
-                echo "<script>window.location.href = 'index.php?action=radminPengajuan';</script>";
-            } else {
-                echo "<script>alert('Terjadi kesalahan pada saat update!');</script>";
-            }
+        $pathKK = $this->processFileUpload('pathKK', $data['pathKK']); 
+        if ($pathKK === false) {
+            echo "<script>alert('Terjadi kesalahan saat upload foto KK!');</script>";
+            return; 
+        }
+
+        $pathRekumendasi = $this->processFileUpload('pathRekumendasi', $data['pathRekumendasi']);
+        if ($pathRekumendasi === false) {
+            echo "<script>alert('Terjadi kesalahan saat upload foto Rekomendasi!');</script>";
+            return;
+        }
+
+        $this->adminPengajuan->id = $id;
+        $this->adminPengajuan->nama = $nama;
+        $this->adminPengajuan->jk = $jk;
+        $this->adminPengajuan->nik = $nik;
+        $this->adminPengajuan->tmpLahir = $tmpLahir;
+        $this->adminPengajuan->tglLahir = $tglLahir;
+        $this->adminPengajuan->alamat = $alamat;
+        $this->adminPengajuan->agama = $agama;
+        $this->adminPengajuan->statusPerkawinan = $statusPerkawinan;
+        $this->adminPengajuan->pekerjaan = $pekerjaan;
+        $this->adminPengajuan->negara = $negara;
+        $this->adminPengajuan->golDarah = $golDarah;
+        $this->adminPengajuan->pathKK = $pathKK;
+        $this->adminPengajuan->pathRekumendasi = $pathRekumendasi;
+
+        if ($this->adminPengajuan->updatePengajuan()) {
+            echo "<script>window.location.href = 'index.php?action=radminPengajuan';</script>";
         } else {
-            $stmt = $this->adminPengajuan->showPengajuan($id);
-            $data = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($data) {
-                include 'app/views/adminPengajuan/update.php';
-            } else {
-                echo "User not found.";
-            }
+            echo "<script>alert('Terjadi kesalahan pada saat update!');</script>";
+        }
+    } else {
+        $stmt = $this->adminPengajuan->showPengajuan($id);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($data) {
+            include 'app/views/adminPengajuan/update.php';
+        } else {
+            echo "User not found.";
         }
     }
+}
 
     public function view($id)
     {
@@ -119,6 +132,30 @@ class adminPengajuanController
         }
     }
 
+        private function processFileUpload($fieldName, $oldFilePath) {
+        if ($_FILES[$fieldName]['error'] === UPLOAD_ERR_OK) {
+            $target_dir = "app/views/assets/images/foto/";
+            $target_file = $target_dir . basename($_FILES[$fieldName]["name"]);
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            var_dump($_FILES);
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                echo "<script>alert('Tipe file $fieldName tidak valid!');</script>";
+                return false;
+            } elseif ($_FILES[$fieldName]["size"] > 2000000) {
+                echo "<script>alert('Ukuran file $fieldName terlalu besar!');</script>";
+                return false;
+            } else {
+                if (move_uploaded_file($_FILES[$fieldName]["tmp_name"], $target_file)) {
+                    return basename($_FILES[$fieldName]["name"]); 
+                } else {
+                    return false; 
+                }
+            }
+        } else {
+            return $oldFilePath; 
+        }
+    }
+
     public function approve($id){
         if ($this->adminPengajuan->approvePengajuan($id)) {
             echo "<script>window.location.href = 'index.php?action=radminPengajuan';</script>"; 
@@ -126,16 +163,5 @@ class adminPengajuanController
             echo "<script>alert('Terjadi kesalahan saat approve pengajuan!');</script>";  
         }
     }
-
-    public function tolak($id){
-        if ($this->adminPengajuan->tolakPengajuan($id)) {
-            echo "<script>window.location.href = 'index.php?action=radminPengajuan';</script>"; 
-        } else {
-            echo "<script>window.location.href = 'index.php?action=radminPengajuan';</script>"; 
-        }
-    }
-
-
-
 
 }
